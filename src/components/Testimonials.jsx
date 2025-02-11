10cc96e8beaffa5c08303b5e50078a2fad6ccb16
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2025-02-11 11:33:11 +0200

    updating the testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -70,12 +70,33 @@ export const TestimonialsSection = () => {
             exit={{ opacity: 0, y: -20 }}
             {...handlers} // Attach swipe handlers
           >
-            <p className="text-gray-700 italic text-xl">
-              &ldquo;{testimonials[currentIndex].feedback}&rdquo;
-            </p>
-            <h3 className="mt-2 font-semibold text-lg">
-              {testimonials[currentIndex].name}
-            </h3>
+            <div className="flex flex-col items-center mx-12 lg:mx-0">
+              <div className="relative text-center">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute top-0 left-0 w-8 h-8 dark:text-gray-300"
+                >
+                  <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                  <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                </svg>
+                <p className="px-6 py-1 text-lg italic">
+                  {testimonials[currentIndex].feedback}
+                </p>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 512 512"
+                  fill="currentColor"
+                  className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-300"
+                >
+                  <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                  <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                </svg>
+              </div>
+              <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-600"></span>
+              <h4> {testimonials[currentIndex].name}</h4>
+            </div>
           </motion.div>
         </div>
       </div>
