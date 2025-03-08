d1890e1f73451344b70c0f5e6f1cb4d8dcfa4f2a
Tapiwanashe Chakurungama <chakurungamatapiwanashe@gmail.com>
2025-03-08 09:58:08 +0200

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './pages/Home'
-import About from './pages/About'
-import Products from './pages/Products'
-import Prescriptions from './pages/Prescriptions'
-import { NavbarComponent }from './components/Navbar'
-import Contact from './pages/Contact'
-import { Footer } from './components/Footer'
-import Cart from './pages/Cart'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Prescriptions from "./pages/Prescriptions";
+import { NavbarComponent } from "./components/Navbar";
+import Contact from "./pages/Contact";
+import { Footer } from "./components/Footer";
+import Cart from "./pages/Cart";
+import { useEffect, useState } from "react";
+import RingLoader from "react-spinners/RingLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 4000);
+  }, []);
 
   return (
     <>
-      <BrowserRouter>
-        <NavbarComponent></NavbarComponent>
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
-          <Route path="/contact" element={<Contact />}></Route>
-          <Route path="/products" element={<Products />}></Route>
-          <Route path="/cart" element={<Cart />}></Route>
-          <Route path="/prescriptions" element={<Prescriptions />}></Route>
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      {loading ? (
+        <div className="w-full h-[100vh] flex justify-center items-center ">
+          <RingLoader />
+        </div>
+      ) : (
+        <BrowserRouter>
+          <NavbarComponent></NavbarComponent>
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/about" element={<About />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+            <Route path="/products" element={<Products />}></Route>
+            <Route path="/cart" element={<Cart />}></Route>
+            <Route path="/prescriptions" element={<Prescriptions />}></Route>
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      )}
     </>
   );
 }
 
-export default App
+export default App;
